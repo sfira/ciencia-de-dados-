c7db71ec4975fe70717f79a75a31233ecbbbf4d5
sfira <114525451+sfira@users.noreply.github.com>
2024-09-13 11:09:06 -0300

    Create common.js

---
+++ common.js
@@ -0,0 +1,24 @@
+</footer>
+<script type="module" src="graficos/common.js"></script>
+<script type="module" src="graficos/informacoesGlobais.js"></script>
+<script type="module" src="graficos/quantidadeUsuarios.js"></script>
+const getCSS = (variavel) => {
+  return getComputedStyle(document.body).getPropertyValue(variavel)
+};
+
+export {getCSS}
+import { getCSS } from "./common.js";
+const data = [
+  {
+    x: 'nomeDasRedes',
+    y: quantidadeDeUsuarios,
+    type: 'bar',
+    marker: {
+      color: getCSS('--primary-color')
+    }
+  }
+]
+const layout = {
+    plot_bgcolor: getCSS('--bg-color')
+    paper_bgcolor: getCSS('--bg-color')
+}
