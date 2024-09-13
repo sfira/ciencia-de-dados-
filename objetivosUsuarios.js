30c3a99b52f42b17aa6591a0714e87f988d7fa07
sfira <114525451+sfira@users.noreply.github.com>
2024-09-13 11:05:54 -0300

    Update objetivosUsuarios.js

--- objetivosUsuarios.js
+++ objetivosUsuarios.js
@@ -1,1 +1,30 @@
+async function objetivosUsuarios() {
+  const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/razoes-globais.json'
+  const res = await fetch(url)
+  const dados = await res.json()
+  const objetivosUsuarios = Object.keys(dados)
+  const quantidadeUsuarios = Object.values(dados)
+
+const data = [
+  {
+    labels: objetivosUso,
+    values: quantidadeUsuarios,
+    type: 'pie'
+  }
+]
+
+const layout = 
+  {
+    height: 600,
+    width: 870,
+  }
+
+const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data, layout)
+
+}
+
+objetivosUsuarios()
 
