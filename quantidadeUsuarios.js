2c59d7031bba93a966e6332462852ed7c4b5a8e7
sfira <114525451+sfira@users.noreply.github.com>
2024-09-13 10:43:32 -0300

    Update quantidadeUsuarios.js

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -7,4 +7,17 @@ async function quantidadeUsuarios() {
 }
 quantidadeUsuarios()
 {"Facebook": 3049, "Youtube": 2491, "WhatsApp": 2000, "Instagram": 2000, "TikTok": 1562}
+const nomeDasRedes = Object.keys(dados)
+const quantidadeUsuarios = Object.values(dados)
+const data = [
+  {
+    x: nomeDasRedes,
+    y: quantidadeUsuarios,
+    type: 'bar'
+  }
+]
+const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data)
 
