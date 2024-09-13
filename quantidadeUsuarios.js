d84bc6159c637ccf632d554e13a4d6d895ce7f24
sfira <114525451+sfira@users.noreply.github.com>
2024-09-13 10:59:33 -0300

    Update quantidadeUsuarios.js

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,14 +1,11 @@
+{"Facebook": 3049, "Youtube": 2491, "WhatsApp": 2000, "Instagram": 2000, "TikTok": 1562}
 async function quantidadeUsuarios() {
-  const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
+  const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
   const res = await fetch(url)
   const dados = await res.json()
-    
-    console.log(dados)
-}
-quantidadeUsuarios()
-{"Facebook": 3049, "Youtube": 2491, "WhatsApp": 2000, "Instagram": 2000, "TikTok": 1562}
-const nomeDasRedes = Object.keys(dados)
-const quantidadeUsuarios = Object.values(dados)
+  const nomeDasRedes = Object.keys(dados)
+  const quantidadeUsuarios = Object.values(dados)
+
 const data = [
   {
     x: nomeDasRedes,
@@ -16,15 +13,15 @@ const data = [
     type: 'bar'
   }
 ]
+
 const grafico = document.createElement('div')
 grafico.className = 'grafico'
 document.getElementById('graficos-container').appendChild(grafico)
 Plotly.newPlot(grafico, data)
-async function quantidadeUsuarios() {
-  const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
-  const res = await fetch(url)
-  const dados = await res.json()
+
 }
 
 quantidadeUsuarios()
+
+      
 
