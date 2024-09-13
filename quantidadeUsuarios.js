8700e8d3a7db82857c4fe6a45bd873249a6c9d23
sfira <114525451+sfira@users.noreply.github.com>
2024-09-13 11:12:16 -0300

    Update quantidadeUsuarios.js

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -5,12 +5,14 @@ async function quantidadeUsuarios() {
   const dados = await res.json()
   const nomeDasRedes = Object.keys(dados)
   const quantidadeUsuarios = Object.values(dados)
-
 const data = [
   {
-    x: nomeDasRedes,
-    y: quantidadeUsuarios,
-    type: 'bar'
+    x: 'nomeDasRedes',
+    y: quantidadeDeUsuarios,
+    type: 'bar',
+    marker: {
+      color: getComputedStyle(document.body).getPropertyValue('--primary-color')
+    }
   }
 ]
 
