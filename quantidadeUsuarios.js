f2aac79667cb5c3782362c9b90372279176bf84d
sfira <114525451+sfira@users.noreply.github.com>
2024-09-13 10:41:37 -0300

    Update quantidadeUsuarios.js

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,1 +1,10 @@
+async function quantidadeUsuarios() {
+  const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
+  const res = await fetch(url)
+  const dados = await res.json()
+    
+    console.log(dados)
+}
+quantidadeUsuarios()
+{"Facebook": 3049, "Youtube": 2491, "WhatsApp": 2000, "Instagram": 2000, "TikTok": 1562}
 
