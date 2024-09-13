fd52e758e5243d3bb20470c57a63e47e83fcdeff
sfira <114525451+sfira@users.noreply.github.com>
2024-09-13 11:16:52 -0300

    Update quantidadeUsuarios.js

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -24,6 +24,23 @@ Plotly.newPlot(grafico, data)
 }
 
 quantidadeUsuarios()
+// código omitido
+
+const layout = {
+  plot_bgcolor: getCSS('--bg-color'),
+  paper_bgcolor: getCSS('--bg-color'),
+  title: {
+    text: 'Redes sociais com mais usuários no mundo',
+     x: 0,
+     font: {
+         color: getCSS('--primary-color'),
+         family: getCSS('--font'),
+         size: 30
+     }
+  }
+}
+
+// código omitido
 
       
 
