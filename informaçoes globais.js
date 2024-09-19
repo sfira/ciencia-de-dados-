32354d785640c465c5ba5e5b88a9c2e8cd2bca0d
sfira <114525451+sfira@users.noreply.github.com>
2024-09-19 11:00:31 -0300

    Update informaçoes globais.js

--- informaçoes globais.js
+++ informaçoes globais.js
@@ -51,3 +51,7 @@ async function vizualizarInformacoesGlobais() {
 }
 
 vizualizarInformacoesGlobais()
+
+const paragrafo = document.createElement('p');
+paragrafo.classList.add('graficos-container_texto')
+paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`;
