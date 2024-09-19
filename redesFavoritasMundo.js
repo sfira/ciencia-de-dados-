8dd180d758ea2f04edcc2274adbb228fa88a1918
sfira <114525451+sfira@users.noreply.github.com>
2024-09-19 10:48:18 -0300

    Update redesFavoritasMundo.js

--- redesFavoritasMundo.js
+++ redesFavoritasMundo.js
@@ -1,1 +1,18 @@
+async function redesFavoritasMundo() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const redes = Object.keys(dados)
+    const valores = Object.values(dados)
+  console.log(dados);
+   const data = [
+        {
+            values: valores,
+            labels: redes,
+            type: 'pie',
+            textinfo: 'label+percent'
+        }
+    ]
+}
 
+redesFavoritasMundo()
