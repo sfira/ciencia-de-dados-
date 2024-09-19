d41edd1c6a2eddb4d7c251f9f57871a9d1e70def
sfira <114525451+sfira@users.noreply.github.com>
2024-09-19 11:29:25 -0300

    Update and rename minha-escola.html to redeSociaisMinhaEscola.js

--- redeSociaisMinhaEscola.js
+++ redeSociaisMinhaEscola.js
@@ -29,3 +29,22 @@
     <script type="module" src="graficos/redesFavoritasMundo.js"></script>
 </body>
 </html>
+<!-- código omitido -->
+
+    <script type="module" src="graficos/redesSociaisMinhaEscola.js"></script>
+</body>
+</html>
+async function redesSociaisFavoritasMinhaEscola() {
+    const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=rSe23zaQC7gOvWgFJbdtPaqh7ewsO5hQmusYOeqdorTRN8C25vVV3BicsPo6HS3jnJY9NNhy_pNZj6prQdxDH3305Mro8vNm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1nSncGQajx_ryfhECjZEnPvESZ9fvnAeFWqfIvIacdoRZcVMZ-nDSydw9_0gseo2TN3y60rOTtwDBCYnKQf6yIqgf8yOzNfccjP633C9VnHmUmPZvRBJY9z9Jw9Md8uu&lib=MCARBaBtNBMHKiEwMeRap3j6V_G7SlGWF'
+    const res = await fetch(url) 
+    const dados = await res.json()
+
+    const redesSociais = dados.slice(1).map(redes = redes[1])
+    console.table(dados)
+}
+
+redesSociaisFavoritasMinhaEscola()
+const contagemRedesSociais = redesSociais.reduce((acc, redesSociais) => {
+    acc[redesSociais] = (acc[redesSociais] || 0) + 1
+    return acc
+}, {})
