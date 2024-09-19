10fad3b1533cdd19ae9d160eba6e69ed1c4125f3
sfira <114525451+sfira@users.noreply.github.com>
2024-09-19 10:56:37 -0300

    Update redesFavoritasMundo.js

--- redesFavoritasMundo.js
+++ redesFavoritasMundo.js
@@ -16,3 +16,37 @@ async function redesFavoritasMundo() {
 }
 
 redesFavoritasMundo()
+import { getCSS } from "./common.js"
+// código omitido
+
+const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+        text: 'Redes sociais que os usuários mais gostam',
+        x: 0,
+        font: {
+            color: getCSS('--primary-color'),
+            family: getCSS('--font'),
+            size: 30
+        }
+    },
+    legend: {
+        font: {
+            color: getCSS('--primary-color'),
+            size: 16
+        }
+    }
+}
+const grafico = document.createElement('div');
+grafico.className = 'grafico';
+function criarGrafico(data, layout) {
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    const config = {
+        responsive: true,
+        displayModeBar: false
+    }
+    Plotly.newPlot(grafico, data, layout, config);
+}
